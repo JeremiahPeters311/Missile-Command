837f6ed96acc6863f8b8a2a8a19a2a21cecd0e53
JeremiahPeters <jcpeters@mail.bradley.edu>
2022-09-11 23:46:04 -0500

    a

--- Assets/Scripts/ExplosionBehaviour.cs
+++ Assets/Scripts/ExplosionBehaviour.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 
 public class ExplosionBehaviour : MonoBehaviour
 {
+    public Animator Anim;
+    public BoxCollider2D Bcol;
     void Start()
     {
         StartCoroutine(Die());
     }
     IEnumerator Die()
     {
-        yield return new WaitForSeconds(0.745f);
+        Anim = gameObject.GetComponent<Animator>();
+        Bcol = gameObject.GetComponent<BoxCollider2D>();
+        //Anim.speed = 0;
+
+        yield return new WaitForSeconds(0.1f);
+        Bcol.size = new Vector2(0.2f, 0.2f);
+        yield return new WaitForSeconds(0.3f);
+        Bcol.size = new Vector2(0.1f, 0.1f);
+        yield return new WaitForSeconds(0.345f);
         Destroy(gameObject);
         yield return null;
     }
